2cf953712938eae709868435bc3753e40443ac51
Bron LaPoint <gemini2022@yahoo.com>
2024-06-18 13:25:13 -0400

    Created data grid library

---
+++ projects/data-grid/src/lib/data-grid.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-data-grid',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      data-grid works!
+    </p>
+  `,
+  styles: ``
+})
+export class DataGridComponent {
+
+}
